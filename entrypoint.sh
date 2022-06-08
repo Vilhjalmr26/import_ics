ec04d6c113aa9014f2127639db9801a1fb4f9d3b
fritz <fritz@vilhjalmr.com>
2022-06-08 01:06:56 -0500

    Fix permissions on config file so it is editable, owner of export folder, and crontab command for changed location.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,13 @@
 if ! [ -f "/config/calendar_download_map.py" ];
 then
     cp /import_ics/samples/calendar_download_map.py /config/calendar_download_map.py
+    chmod 777 /config/calendar_download_map.py
 fi
 
 cd /import_ics
-echo "${CRONTAB_SCHEDULE} /import_ics/import_ics.py" > crontab
+echo "${CRONTAB_SCHEDULE} /import_ics.sh" > crontab
+
+chown root:root /ical_export
 
 crontab crontab
 
